cb04be5a57ab32edd494c0642c261045e08640c2
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-03-18 22:53:15 -0500

    FIX - Ajuste pruebas servicio y maquetación.

--- lib/gui/views/home_view/home_view.dart
+++ lib/gui/views/home_view/home_view.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:prueba_tecnica_inlaze/gui/app_style.dart';
 
-class HomeView extends StatelessWidget {
+import 'package:http/http.dart' as http;
+
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getBooks();
+  }
+
+  getBooks() async {
+    // https://api.itbook.store/1.0/new
+    final url = Uri.https('api.itbook.store', '1.0/new');
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +37,7 @@ class HomeView extends StatelessWidget {
       body: GestureDetector(
         onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
@@ -42,7 +66,18 @@ class HomeView extends StatelessWidget {
                 ],
               ),
             ),
-        
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                    child: Text("Nuevos libros")
+                  ),
+                  Text("Libros..")
+                ],
+              ),
+            )
           ],
         ),
       )
