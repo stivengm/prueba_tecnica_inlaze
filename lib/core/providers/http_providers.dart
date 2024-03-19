97cffad02ef051726580fbfc58255554d778f2c0
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-03-19 13:10:27 -0500

    FIX - Ajuste de http_provider.

--- lib/core/providers/http_providers.dart
+++ lib/core/providers/http_providers.dart
@@ -3,16 +3,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class HttpProvider<T> {
-
-  String? endPoint;
+  
+  final String endPoint;
 
   HttpProvider({ required this.endPoint });
 
-  Uri _url () => Uri.https('api.itbook.store', '$endPoint');
-
   Future<T?> get() async {
+    final Uri url = Uri.https('api.itbook.store', endPoint);
+
     try {
-      final response = await http.get(_url as Uri);
+      final response = await http.get(url);
       return (response.statusCode == 200) ? jsonDecode(response.body) : null;
     } catch (e) {
       return null;
