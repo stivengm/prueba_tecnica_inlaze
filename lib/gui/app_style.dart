014bf8d4d1adda3ab0c02f79aaae9a21fdbdd45f
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-03-18 22:52:56 -0500

    FIX - Ajuste de color de fondo para el app.

--- lib/gui/app_style.dart
+++ lib/gui/app_style.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xFFF67952);
-const Color backgroundApp = Color(0xFFFBFBFD);
+const Color backgroundApp = Color(0xFFF8F7F9);
 const Color whiteColor = Colors.white;
