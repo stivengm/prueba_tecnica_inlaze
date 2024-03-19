3fd3720da02f2b2a278064e6e456567c5a6fba3b
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-03-18 21:42:53 -0500

    FIX - Ajuste de paths.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prueba_tecnica_inlaze/core/config/app_paths.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder()
+      routes: routesApp(),
+      initialRoute: 'login'
     );
   }
 }
