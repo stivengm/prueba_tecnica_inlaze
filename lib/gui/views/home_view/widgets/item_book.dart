3c19e1b135c26508a1ad335b0fb73e18118a2877
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-03-19 21:23:06 -0500

    FIX - Ajuste de icono favorite.

--- lib/gui/views/home_view/widgets/item_book.dart
+++ lib/gui/views/home_view/widgets/item_book.dart
@@ -24,24 +24,46 @@ class ItemBook extends StatelessWidget {
       ),
       margin: const EdgeInsets.only(top: 10.0, right: 6.0, bottom: 20.0, left: 6.0),
       padding: const EdgeInsets.symmetric(horizontal: 5.0),
-      child: Column(
+      child: Stack(
         children: [
-          SizedBox(
-            width: 180.0,
-            height: 198,
-            child: Hero(
-              tag: book.isbn13,
-              child: FadeInImage(
-                fadeInDuration: const Duration(milliseconds: 200),
-                placeholder: const AssetImage('assets/loader_image.gif'),
-                image: NetworkImage(book.image),
+          Column(
+            children: [
+              SizedBox(
+                width: 180.0,
+                height: 198,
+                child: Hero(
+                  tag: book.isbn13,
+                  child: FadeInImage(
+                    fadeInDuration: const Duration(milliseconds: 200),
+                    placeholder: const AssetImage('assets/loader_image.gif'),
+                    image: NetworkImage(book.image),
+                  ),
+                ),
               ),
-            ),
+              Text(book.price, style: Theme.of(context).textTheme.titleMedium!.copyWith( fontWeight: FontWeight.bold, fontSize: 15.0 )),
+              Expanded(
+                child: Text(book.title, style: Theme.of(context).textTheme.headlineMedium!.copyWith( fontWeight: FontWeight.w400, fontSize: 15.0 ), textAlign: TextAlign.center,)
+              )
+            ],
           ),
-          Text(book.price, style: Theme.of(context).textTheme.titleMedium!.copyWith( fontWeight: FontWeight.bold, fontSize: 15.0 )),
-          Expanded(
-            child: Text(book.title, style: Theme.of(context).textTheme.headlineMedium!.copyWith( fontWeight: FontWeight.w400, fontSize: 15.0 ), textAlign: TextAlign.center,)
-          )
+          Positioned(
+            top: 5,
+            right: 0,
+            child: Container(
+              width: 40.0,
+              height: 40.0,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(.5),
+                borderRadius: BorderRadius.circular(100)
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.heart_broken
+                ),
+              ),
+            )
+          ),
         ],
       ),
     );
